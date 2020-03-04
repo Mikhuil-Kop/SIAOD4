e32358fa35c929e2202652558a7261478315454b
Mikhuil-Kop <44004552+Mikhuil-Kop@users.noreply.github.com>
2020-03-04 18:01:14 +0300

    Лаба 2

--- Lab2_13/Lab2_13/graph.cpp
+++ Lab2_13/Lab2_13/graph.cpp
@@ -1,4 +1,5 @@
 #include "graph.hpp"
+#include "list"
 
 graph::graph(bool has_weight, int size){
     this->has_weight = has_weight;
@@ -6,6 +7,10 @@ graph::graph(bool has_weight, int size){
     edges = new int*[size];
     for(int i = 0; i < size; i++)
         edges[i] = new int[size];
+    
+    for(int i = 0; i < size; i++)
+        for(int j = 0; j < size; j++)
+            edges[i][j] = 0;
 }
 graph::~graph(){
     for(int i = 0; i < size; i++)
@@ -34,12 +39,6 @@ ostream &operator<<(ostream &out, const graph &g){
     return out;
 }
 istream &operator>>(istream &in, graph &g){
-    in >> g.size;
-    
-    g.edges = new int*[g.size];
-    for(int i = 0; i < g.size; i++)
-        g.edges[i] = new int[g.size];
-    
     for(int i = 0; i < g.size; i++){
         g.edges[i][i] = 0;
         for(int j = i + 1; j < g.size; j++){
@@ -83,10 +82,51 @@ struct str{
 public:
     str(int w, int x, int y):w(w), x(x), y(y){}
     int w, x, y;
+    
+    friend bool operator>(const str& s1, const str& s2){return s1.w > s2.w;}
+    friend bool operator<(const str& s1, const str& s2){return s1.w < s2.w;}
+    friend bool operator==(const str& s1, const str& s2){return s1.w == s2.w;}
 };
 
 graph graph::get_frame(){
     graph exit = graph(true, this->size);
+    
+    list<str> l;
+    
+    for(int i = 0; i < size; i++)
+        for(int j = i + 1; j < size; j++)
+            if(edges[i][j] != 0)
+                l.push_back(str(edges[i][j],i,j));
+    l.sort();
+    
+    //список отдельных каркасов
+    list<list<int>> frames;
+    for(int i = 0; i < size; i++)
+        frames.push_back(list<int>{i});
+                         
+    //заполнение итогового каркаса и слияние списков
+    while(!l.empty()){
+        str now = l.front();
+        l.pop_front();
+        
+        //поиск и слияние
+        for(list<int>& f : frames){
+            bool c1 = false, c2 = false;
+            //нахождение узлов в списке
+            for(int i: f){
+                if(i == now.x)
+                    c1 = true;
+                if(i == now.y)
+                    c2 = true;
+            }
+            //если было повторение
+            if(c1 && c2)
+                break;
+        }
+        
+        exit.edges[now.x][now.y] = now.w;
+        exit.edges[now.y][now.x] = now.w;
+    }
     
     return exit;
 }
--- Lab2_13/Lab2_13/main.cpp
+++ Lab2_13/Lab2_13/main.cpp
@@ -3,14 +3,18 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    graph g(true);
+    cout << "Введите длину\n";
+    int len;
+    cin >> len;
+    
+    graph g(true, len);
     cout << "Введите количество узлов графа, а потом их веса:\n";
     cin >> g;
     
-    cout << "\nПолучившыйся граф:\n";
+    cout << "\nПолучившийся граф:\n";
     cout << g;
     
-    cout << "\nЕго каркас";
+    cout << "\nЕго каркас\n";
     cout << g.get_frame();
     return 0;
 }
